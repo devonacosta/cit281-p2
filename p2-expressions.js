f2cc76943f8c2422038ca71f1eae26514389d1a6
devonacosta <105311604+devonacosta@users.noreply.github.com>
2022-06-03 11:29:57 -0700

    Add files via upload

---
+++ p2-expressions.js
@@ -0,0 +1,39 @@
+/*
+    CIT 281 Project 2
+    Name: Devon Acosta
+*/
+
+ // Returns a random number 
+ function getRandomInteger() {
+    return Math.floor(Math.random());
+}
+
+// returns a random letter from alphabet constant
+let getRandomLetter = function(min, max) {
+    return alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+
+ // returns a random length string between minLength and maxLength
+let getRandomString = function(minLength, maxLength) {
+    return Math.floor(Math.random()) * (maxLength - minLength) + minLength;
+}
+ 
+let result = "";
+
+let lengthOfOutputString = getSortedString();
+
+
+// returns a sorted string in ascending order through an array method
+let getSortedString = function(string) { 
+    return string = "xpacd".split('').sort().join('');
+}
+
+
+for (let i = 0; i < lengthOfOutputString; i++) {
+    result += alphabet[getSortedString(alphabet.length)];
+}
+
+console.log(getSortedString("xpacd"));
